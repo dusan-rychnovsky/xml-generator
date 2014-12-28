4d4a07e1d027cce9dd53987b192113f19b555822
Dusan Rychnovsky <dusan.rychnovsky@gmail.com>
2014-12-28 18:06:17 +0100

    Scala - add a basic test suite skeleton for regexps.

---
+++ scala/src/test/scala/ExpressionSpec.scala
@@ -0,0 +1,15 @@
+import org.scalatest._
+
+class ExpressionSpec extends FunSpec {
+
+  describe("Expression Evaluation") {
+    
+    it("Allows to evaluate an identifier-like expression.") (pending)
+    
+    it("Allows to evaluate a union-like expression.") (pending)
+    
+    it("Allows to evaluate a concatenation-like expression.") (pending)
+    
+    it("Allows to evaluate an interation-like expression.") (pending)
+  }
+}
